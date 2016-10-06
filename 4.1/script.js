e7430e89ed71a03438ce257ea419e13cb1ab609b
Sarah Campbell <sarahsoup@Sarahs-MacBook-Pro.local>
2016-10-06 12:28:10 -0400

    Assignment Submission

--- 4.1/script.js
+++ 4.1/script.js
@@ -7,24 +7,31 @@ var arr1 = [];
 
 //Task 1: populate the empty array with 10 random, *rounded*, numbers between 0 and 100
 //Hint: within a 'for' loop, use 'Math.random() * 100' and 'Math.round()'
-
+for (i=0;i<10;i++){
+  var num = Math.random()*100
+  var numRound = Math.round(num)
+  arr1.push(numRound)
+};
 
 //Task 2: remove the first element in the array and log it in console
 //Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
-
+arr1.shift();
 
 //Task 3: remove the last element in the array, and log it in console
 //Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
-
+arr1.pop();
 
 //Question: how many elements does arr1 have at this moment?
-
+//8
 
 //Task 4: given a new empty array arr2, populate it with the exact same values as in arr1
 //...but in reverse order
 //Hint: use the '.unshift()' method of array. Can you look up the API for it?
 var arr2 = [];
 
+arr1.forEach(function(element){
+  arr2.unshift(element);
+});
 
 //Task 5 OPTIONAL: filter out all the odd values in arr1, keeping the even values
 //Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
--- 4.2/script.js
+++ 4.2/script.js
@@ -5,7 +5,11 @@
 //Create a function such that, given argument arr, an array, it returns the same array in reverse order
 
 function reverseArray(arr){
-    //your code here
-}
+  var arrRev = []
+  for(var i=0; i<arr.length;i++){
+    arrRev.unshift(arr[i]);
+  };
+  console.log(arrRev);
+};
 
 console.log( reverseArray([1,2,3,4,5]) ); //should log [5,4,3,2,1]
